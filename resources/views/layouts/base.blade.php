ca34152fe9ed57dbcfac6094c31f8cae82b01841
unknown <rakotonirinabjr@gmail.com>
2025-04-30 22:08:52 +0300

    create student

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -80,7 +80,7 @@
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav" class="pt-4">
                         <li class="sidebar-item"> 
-                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index.html" aria-expanded="false">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('dashboard') }}" aria-expanded="false">
                                 <i class="mdi mdi-view-dashboard"></i>
                                 <span class="hide-menu">Dashboard</span>
                             </a>
@@ -112,13 +112,13 @@
                         <li class="sidebar-item"> 
                             <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
                                 <i class="mdi mdi-receipt"></i>
-                                <span class="hide-menu">Forms </span>
+                                <span class="hide-menu">Etudiants </span>
                             </a>
                             <ul aria-expanded="false" class="collapse  first-level">
                                 <li class="sidebar-item">
-                                    <a href="form-basic.html" class="sidebar-link">
+                                    <a href="{{ route('students.create') }}" class="sidebar-link">
                                         <i class="mdi mdi-note-outline"></i>
-                                        <span class="hide-menu"> Form Basic </span>
+                                        <span class="hide-menu"> Créer  </span>
                                     </a>
                                 </li>
                                 <li class="sidebar-item">
@@ -205,9 +205,9 @@
                             </a>
                             <ul aria-expanded="false" class="collapse  first-level">
                                 <li class="sidebar-item">
-                                    <a href="authentication-login.html" class="sidebar-link">
+                                    <a href="{{ route('profile.edit') }}" class="sidebar-link">
                                         <i class="mdi mdi-all-inclusive"></i>
-                                        <span class="hide-menu"> Login </span>
+                                        <span class="hide-menu"> Profile edit </span>
                                     </a>
                                 </li>
                                 <li class="sidebar-item">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\PermissionController;
 use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\StudentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,10 +25,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware(['auth','role:admin'])->prefix('admin')->group(function(){
-//     Route::resources('/roles',RoleController::class);
-//     Route::resources('/permissions',PermissionController::class);
-// });
+Route::middleware('auth')->prefix('admin')->group(function(){
+    // Route::resource('/roles',RoleController::class);
+    // Route::resource('/permissions',PermissionController::class);
+    Route::resource('/students', StudentController::class);
+});
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
