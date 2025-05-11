943f7dbc8b90ef937483fbe5968804f3c8b0a0fb
unknown <rakotonirinabjr@gmail.com>
2025-05-11 21:40:11 +0300

    modif kernel pour role et permission

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->prefix('admin')->group(function(){
+Route::middleware(['auth','role:admin'])->prefix('admin')->group(function(){
     // Route::resource('/roles',RoleController::class);
     // Route::resource('/permissions',PermissionController::class);
     Route::resource('/students', StudentController::class);
 });
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth','role:admin'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
