be9c44ece0f12443e575390a278232869a36f496
MSOFT-INFO <rakotonirinabjr@gmail.com>
2025-05-14 15:31:35 +0300

    students form

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -27,40 +27,151 @@
         <div class="card-body wizard-content">
             <form id="example-form" action="#" class="mt-5">
                 <div>
-                    <h3>Account</h3>
+                    <h3>Identité</h3>
                     <section>
-                        <label for="userName">User name *</label>
-                        <input id="userName" name="userName" type="text" class="required form-control">
-                        <label for="password">Password *</label>
-                        <input id="password" name="password" type="text" class="required form-control">
-                        <label for="confirm">Confirm Password *</label>
-                        <input id="confirm" name="confirm" type="text" class="required form-control">
-                        <p>(*) Mandatory</p>
-                    </section>
-                    <h3>Profile</h3>
-                    <section>
-                        <label for="name">First name *</label>
-                        <input id="name" name="name" type="text" class="required form-control">
-                        <label for="surname">Last name *</label>
-                        <input id="surname" name="surname" type="text" class="required form-control">
+                        <label for="genre">Genre *</label>
+                        <div class="form-group d-flex row" style="margin-left: 1px">
+                            <div class="form-check col-md-2">
+                                <input type="radio" class="form-check-input" id="customControlValidation1" name="gender" required checked>
+                                <label class="form-check-label mb-0" for="customControlValidation1">Homme</label>
+                            </div>
+                            <div class="form-check col-md-2">
+                                <input type="radio" class="form-check-input"
+                                    id="customControlValidation2" name="gender" required>
+                                <label class="form-check-label mb-0" for="customControlValidation2">Femme</label>
+                            </div>
+                        </div>
+                        <label for="firstname">Prenom *</label>
+                        <input id="firstname" name="firstname" type="text" class="required form-control">
+                        <label for="lastname">Nom *</label>
+                        <input id="lastname" name="lastname" type="text" class="required form-control">
                         <label for="email">Email *</label>
-                        <input id="email" name="email" type="text" class="required email form-control">
+                        <input id="email" name="email" type="email" class="required email form-control">
+                        <label for="phone">Téléphone *</label>
+                        <input id="phone" name="phone" type="text" class="required phone form-control">
+                        
+                        <p class="text-warning">(*) Champ obligatoire</p>
+                    </section>
+                    <h3>Information</h3>
+                    <section>
+                        <label for="birth">Naissance *</label>
+                        <input id="birth" name="birth" type="date" class="required form-control">
+                        <label for="nationality">Nationalité *</label>
+                        <input id="nationality" name="nationality" type="text" class="required form-control">
+                        
                         <label for="address">Address</label>
                         <input id="address" name="address" type="text" class=" form-control">
                         <p>(*) Mandatory</p>
                     </section>
-                    <h3>Hints</h3>
+                    <h3>Parents</h3>
                     <section>
-                        <ul>
-                            <li>Foo</li>
-                            <li>Bar</li>
-                            <li>Foobar</li>
-                        </ul>
-                    </section>
-                    <h3>Finish</h3>
-                    <section>
-                        <input id="acceptTerms" name="acceptTerms" type="checkbox" class="required">
-                        <label for="acceptTerms">I agree with the Terms and Conditions.</label>
+                        <div class="d-flex col-md-12">
+                            <div class="card col-md-6">
+                                <div class="card-body">
+                                    <h4 class="card-title">Père</h4>
+                                    <div class="form-group row">
+                                        <label for="fname"
+                                            class="col-sm-3 text-end control-label col-form-label">First Name</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="fname"
+                                                placeholder="First Name Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="lname" class="col-sm-3 text-end control-label col-form-label">Last
+                                            Name</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="lname"
+                                                placeholder="Last Name Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="lname"
+                                            class="col-sm-3 text-end control-label col-form-label">Password</label>
+                                        <div class="col-sm-9">
+                                            <input type="password" class="form-control" id="lname"
+                                                placeholder="Password Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="email1"
+                                            class="col-sm-3 text-end control-label col-form-label">Company</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="email1"
+                                                placeholder="Company Name Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="cono1"
+                                            class="col-sm-3 text-end control-label col-form-label">Contact No</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="cono1"
+                                                placeholder="Contact No Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="cono1"
+                                            class="col-sm-3 text-end control-label col-form-label">Message</label>
+                                        <div class="col-sm-9">
+                                            <textarea class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- parent 2 --}}
+                            <div class="card col-md-6">
+                                <div class="card-body">
+                                    <h4 class="card-title">Mère</h4>
+                                    <div class="form-group row">
+                                        <label for="fname"
+                                            class="col-sm-3 text-end control-label col-form-label">First Name</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="fname"
+                                                placeholder="First Name Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="lname" class="col-sm-3 text-end control-label col-form-label">Last
+                                            Name</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="lname"
+                                                placeholder="Last Name Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="lname"
+                                            class="col-sm-3 text-end control-label col-form-label">Password</label>
+                                        <div class="col-sm-9">
+                                            <input type="password" class="form-control" id="lname"
+                                                placeholder="Password Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="email1"
+                                            class="col-sm-3 text-end control-label col-form-label">Company</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="email1"
+                                                placeholder="Company Name Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="cono1"
+                                            class="col-sm-3 text-end control-label col-form-label">Contact No</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="cono1"
+                                                placeholder="Contact No Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="cono1"
+                                            class="col-sm-3 text-end control-label col-form-label">Message</label>
+                                        <div class="col-sm-9">
+                                            <textarea class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </section>
                 </div>
             </form>
@@ -88,6 +199,7 @@
             transitionEffect: "slideLeft",
             onStepChanging: function (event, currentIndex, newIndex) {
                 form.validate().settings.ignore = ":disabled,:hidden";
+                console.log('event',event)
                 return form.valid();
             },
             onFinishing: function (event, currentIndex) {
@@ -95,7 +207,7 @@
                 return form.valid();
             },
             onFinished: function (event, currentIndex) {
-                alert("Submitted!");
+                console.log('event',currentIndex)
             }
         });
 
